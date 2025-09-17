abf4102b20b9185473adb273b396ecb6bf69f602
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-09-17 17:01:00 +0530

    okay

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -91,7 +91,7 @@ const Footer = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 px-2">
           {/* Brand Section - Larger logo */}
           <div className="space-y-2 text-center sm:text-left">
-            <div className="flex-shrink-0 flex items-center">
+            <div className="flex items-center justify-center sm:justify-start">
               <Link
                 href="/"
                 className="flex items-center group transition-all duration-500 ease-in-out"
